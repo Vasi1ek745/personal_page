59812d1c470bc21f8909d946982faccafca4ceeb
Vasi1ek745 <vasi1ek745@gmail.com>
2023-08-06 12:53:43 +0500

    test

---
+++ test/controllers/main_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class MainControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get main_index_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get main_about_url
+    assert_response :success
+  end
+
+  test "should get project" do
+    get main_project_url
+    assert_response :success
+  end
+end
